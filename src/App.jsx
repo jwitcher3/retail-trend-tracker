a0a01463d98c5c9d077ada4318940a31bd5afb4e
James Witcher <jameswitcher@MacBook-Pro.localdomain>
2025-07-28 23:00:03 -0700

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+// src/App.jsx
 import RetailTrendDashboard from "./retailtrenddashboard";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  return <RetailTrendDashboard />;
+  return (
+    <>
+      <RetailTrendDashboard />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
